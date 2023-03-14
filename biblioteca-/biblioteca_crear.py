a78bf34d64e39a1f079b3bee03386aa63553da4a
main <salazarquintero04@gmail.com>
2023-03-13 19:14:34 -0500

    agregarlo

---
+++ biblioteca-/biblioteca_crear.py
@@ -0,0 +1,42 @@
+# #este programa te dice el libro que quieres adquirir en una biblioteca con tu nombre
+# from biblioteca_crear  import * 
+# from biblioteca_actualizar import *
+
+# bili={}
+
+# while True:
+#    print("*"*70)
+#    menu = int(input("""Este menu es para oder adquirir un libro en una biblioteca 
+#       [0]  crear un usuario nuevo 
+#       [1]  actualizar el usuario
+#       [2]  visualizar el usuario
+#       [3]  eliminar el usuario
+#       [4]  cerrar 
+#       -INGRESA EL NUMERO:
+#       """))
+#       if menu == 0:
+#       agregar_usuario(bili)
+#     elif menu == 1:
+#       actualizar_usuario(bili)
+#     elif menu == 2:
+#       print(bili)
+#     elif  menu == 3:
+#      eliminar_usuario(bili)
+#     elif  menu == 4:
+#       break
+#    print("-"*50)
+# 
+
+
+# class personas():
+#     def __init__(self,nombre,id):
+#           self.nombre=nombre
+#           self.id=id
+#           self.info={
+#               " id":self.id,
+#               "nombre":self.nombre,
+#                "libros":[]
+#             }
+#     def
+
+print("perro")
